8efd8ba33c82918dbc84eb8858b2d5df12ad5573
Noelia Pires <marianoeliapires@gmail.com>
2026-03-16 13:02:20 +0100

    Punto 5 experiencia final

--- src/sections/Experiencia.jsx
+++ src/sections/Experiencia.jsx
@@ -16,14 +16,15 @@ function Experiencia() {
             </p>
 
 
-            <div className="orbital-layout">
+            <div className="timeline">
 
+                <div className="linea"></div>
 
-                {/* EXPERIENCIA */}
+                <div className="item">
 
-                <div className="lado experiencia-izquierda">
+                    <div className="nodo"></div>
 
-                    <div className="bloque">
+                    <div className="contenido">
 
                         <p className="rol">Resolutions Generalist</p>
                         <p className="empresa">IKEA · 2025</p>
@@ -36,8 +37,14 @@ function Experiencia() {
 
                     </div>
 
+                </div>
 
-                    <div className="bloque">
+
+                <div className="item">
+
+                    <div className="nodo"></div>
+
+                    <div className="contenido">
 
                         <p className="rol">Dependienta · Atención al cliente</p>
                         <p className="empresa">Cinesa · 2014–2025</p>
@@ -53,26 +60,16 @@ function Experiencia() {
                 </div>
 
 
-                {/* NÚCLEO CENTRAL */}
+                <div className="item">
 
-                <div className="nucleo">
+                    <div className="nodo"></div>
 
-                    <div className="halo"></div>
-                    <div className="core"></div>
-
-                </div>
-
-
-                {/* SOPORTE IT */}
-
-                <div className="lado experiencia-derecha">
-
-                    <div className="bloque">
+                    <div className="contenido">
 
                         <p className="rol">Soporte IT</p>
                         <p className="empresa">Competencias aplicadas al soporte técnico</p>
 
-                        <ul className="tareas soporte">
+                        <ul className="tareas">
                             <li>Diagnóstico estructurado de problemas</li>
                             <li>Priorización y organización de tareas</li>
                             <li>Comunicación clara con el usuario</li>
@@ -84,14 +81,12 @@ function Experiencia() {
 
                 </div>
 
-
             </div>
 
 
             <p className="frase-experiencia">
                 “En entornos técnicos, combino base en sistemas con capacidad de comunicación y resolución estructurada de incidencias.”
             </p>
-
 
         </section>
 
